db9771c563871718f8d1f9ccf1e54c7f3bc69996
sunilsunny101 <sunnysunil962@gmail.com>
2024-11-22 13:19:02 +0530

    Adding main file

---
+++ run.py
@@ -0,0 +1,4 @@
+from transformations.code import main  # Import the main function from main.py
+
+if __name__ == "__main__":
+    main()  # Call the main function
--- transformations/Functions_pyspark.py
+++ transformations/Functions_pyspark.py
@@ -41,7 +41,7 @@ def no_of_males_killed(df):
                                .agg(sum('DEATH_CNT').alias('MALE_DEATH_CNT')) \
                                .filter('MALE_DEATH_CNT > 2') \
                                .agg(count('CRASH_ID').alias('num_male_killed'))
-        return num_males_killed_df.show()
+        return num_males_killed_df
     return None
 
 def Two_wheeler_crashes(df):
@@ -57,7 +57,7 @@ def Two_wheeler_crashes(df):
     if df is not None:
         two_wheelers_df = df.filter(col('VEH_BODY_STYL_ID').like('%MOTORCYCLE%')) \
                             .agg(count('CRASH_ID').alias('Motorcycle_crash_count'))
-        return two_wheelers_df.show()
+        return two_wheelers_df
     return None
 
 def Top_5_Veh_Makers(unitdf, persondf):
@@ -79,7 +79,7 @@ def Top_5_Veh_Makers(unitdf, persondf):
                                             .agg(count('e1.CRASH_ID').alias('crash_count')) \
                                             .orderBy(col('crash_count').desc()) \
                                             .limit(5)
-        return top_5_vehicle_makes_df.show()
+        return top_5_vehicle_makes_df
     return None
 
 def Licence_valid_HNR(unitdf, persondf):
@@ -98,7 +98,7 @@ def Licence_valid_HNR(unitdf, persondf):
         valid_licence_hit_run_df = unit_primary_joined_df.filter((col('e2.DRVR_LIC_TYPE_ID') == 'DRIVER LICENSE') &
                                                                   (col('e1.VEH_HNR_FL') == 'Y')) \
                                                           .agg(count('e1.CRASH_ID').alias('HNR_COUNT'))
-        return valid_licence_hit_run_df.show()
+        return valid_licence_hit_run_df
     return None
 
 def State_highest_nonFemale_crash(df):
@@ -117,7 +117,7 @@ def State_highest_nonFemale_crash(df):
                                         .agg(count(col('CRASH_ID')).alias('No_of_accidents')) \
                                         .orderBy(col('No_of_accidents').desc()) \
                                         .limit(1)
-        return state_highest_accidents_df.show()
+        return state_highest_accidents_df
     return None
 
 def top_3_to_5_VEH_Makers(df):
@@ -139,7 +139,7 @@ def top_3_to_5_VEH_Makers(df):
         top_3_to_5_veh = large_no_of_injuries.withColumn('rank', dense_rank().over(window_spec)) \
                                                .filter(col('rank').between(3, 5)) \
                                                .select('VEH_MAKE_ID', 'INJURY_DEATH_CNT')
-        return top_3_to_5_veh.show()
+        return top_3_to_5_veh
     return None
 
 def top_body_ethnic_grp(unitdf, persondf):
@@ -159,7 +159,7 @@ def top_body_ethnic_grp(unitdf, persondf):
                                                          .agg(count(col('e1.CRASH_ID')).alias('ETHNICITY_COUNT')) \
                                                          .orderBy(col('ETHNICITY_COUNT').desc()) \
                                                          .limit(1)
-        return top_ethnic_user_group_df.show()
+        return top_ethnic_user_group_df
     return None
 
 def Top_5_zip_alcoholic(df):
@@ -179,7 +179,7 @@ def Top_5_zip_alcoholic(df):
                                         .orderBy(col('ACCIDENT_COUNT').desc()) \
                                         .filter(col('DRVR_ZIP').isNotNull()) \
                                         .limit(5)
-        return top_5_zip_codes_alcohol_df.show()
+        return top_5_zip_codes_alcohol_df
     return None
 
 def No_damageprop_high_vehdamage(unitdf, damagesdf):
@@ -199,7 +199,7 @@ def No_damageprop_high_vehdamage(unitdf, damagesdf):
                                              .filter(col('e1.FIN_RESP_PROOF_ID') == 1) \
                                              .filter(col('e1.VEH_DMAG_SCL_1_ID').isin(['DAMAGED 5', 'DAMAGED 6'])) \
                                              .agg(count('CRASH_ID').alias('NO_DAMAGE_COUNT'))
-        return distinct_crash_ids_df.show()
+        return distinct_crash_ids_df
     return None
 
 def top_5_vehmak_lic(unitdf, persondf):
@@ -238,5 +238,5 @@ def top_5_vehmak_lic(unitdf, persondf):
                                         .orderBy(col('crash_count').desc())
                                         .limit(5))
         
-        return top_5_vehicle_makes_speed_df.show()
+        return top_5_vehicle_makes_speed_df
     return None
